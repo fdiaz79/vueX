cf2558a9b64412469fe5b404128b41c7ff9dc9c5
Fabian Diaz <16745697+fdiaz79@users.noreply.github.com>
2019-11-15 09:27:16 -0500

    Initial basic App

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <product-list-one v-bind:products="products" ></product-list-one>
+    <product-list-two v-bind:products="products" ></product-list-two>
+  </div>
+</template>
+
+<script>
+import ProductListOne from './components/ProductListOne.vue';
+import ProductListTwo from './components/ProductListTwo.vue';
+export default {
+  components: {
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo
+  },
+  name: 'app',
+  data () {
+    return {
+      products: [
+        {id: 1, name: 'Banana Skin', price: 20},
+        {id: 2, name: 'Shiny Star', price: 40},
+        {id: 3, name: 'Green Shells', price: 60},
+        {id: 4, name: 'Red Shells', price: 40}
+      ]
+    }
+  }
+  
+}
+</script>
+
+<style>
+  body{
+    font-family: Ubuntu;
+    color: #555;
+  }
+
+</style>
