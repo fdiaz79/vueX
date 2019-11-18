f6e1135700a2c8f8d2e106cd431ab2dd78cfec4e
Fabian Diaz <16745697+fdiaz79@users.noreply.github.com>
2019-11-18 10:39:26 -0500

    create getter functions on store and retireve as a computed in the component

--- src/store/store.js
+++ src/store/store.js
@@ -11,6 +11,18 @@ export const store = new Vuex.Store({
             {id: 3, name: 'Green Shells', price: 60},
             {id: 4, name: 'Red Shells', price: 40}
         ]
+    },
+    getters: {
+        saleProducts: state => {
+            var saleProducts = state.products.map( product => {
+                return {
+                    id: product.id,
+                    name: '**' + product.name + '**',
+                    price: product.price/2
+                }
+            });
+            return saleProducts;
+        }
     }
 });
 
